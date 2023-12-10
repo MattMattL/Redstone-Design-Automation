8abc38e969ddb76a7773cd667501b4ebf02d590b
Matt L <primemuon@gmail.com>
2023-12-10 16:17:56 +0000

    Add vertical bar that highlights mouse position

--- src/main/java/net/muonalpha/caverse/tools/timing/WaveformScreen.java
+++ src/main/java/net/muonalpha/caverse/tools/timing/WaveformScreen.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 public class WaveformScreen extends AbstractContainerScreen<WaveformMenu>
 {
 	private static final ResourceLocation WAVEFORM_TEXTURE = new ResourceLocation(CaversMode.MODID, "textures/gui/waveform_gui_texture.png");
-	private ArrayList<Probe> allProbes = new ArrayList<>();
+	private static final ArrayList<Probe> allProbes = new ArrayList<>();
 
 	private int MARGIN;
 
@@ -21,6 +21,7 @@ public class WaveformScreen extends AbstractContainerScreen<WaveformMenu>
 	private int CHANNELS_LEFT;
 	private int CHANNELS_RIGHT;
 	private int CHANNELS_TOP;
+	private int CHANNELS_BOTTOM;
 
 	private int HIGH_WAVE_HEIGHT;
 	private int LOW_WAVE_HEIGHT;
@@ -29,7 +30,9 @@ public class WaveformScreen extends AbstractContainerScreen<WaveformMenu>
 
 	private final float DRAG_Y_MULTIPLIER = 1;
 	private final float SCROLL_MULTIPLIER = 2;
-	private float scrollAmount = 0;
+	private int HOR_SCROLL_LIMIT = 0;
+
+	private float scrollAmountX = 0;
 	private float dragAmountX = 0;
 	private float dragAmountY = 0;
 
@@ -49,6 +52,7 @@ public class WaveformScreen extends AbstractContainerScreen<WaveformMenu>
 
 		this.MARGIN = 6;
 		this.CHANNELS_TOP = this.topPos + this.MARGIN;
+		this.CHANNELS_BOTTOM = this.CHANNELS_TOP + 6 * this.CHANNEL_HEIGHT;
 		this.CHANNEL_HEIGHT = 26;
 		this.CHANNEL_WIDTH = this.imageWidth - 2 * this.MARGIN;
 		this.CHANNELS_LEFT = this.leftPos + this.MARGIN;
@@ -59,12 +63,14 @@ public class WaveformScreen extends AbstractContainerScreen<WaveformMenu>
 		this.WAVE_BOTTOM_MARGIN = 5;
 		this.WIDTH_PER_SIGNAL = 4;
 
+		this.HOR_SCROLL_LIMIT = CHANNEL_WIDTH / 2;
+
+		this.titleLabelX = 10000; // TODO: change to rendering blank strings " "
+		this.inventoryLabelY = 10000;
+
+		this.allProbes.clear();
 		this.allProbes.addAll(TimingTool.inputProbes);
 		this.allProbes.addAll(TimingTool.outputProbes);
-
-		// TODO: change to rendering blank strings " "
-		this.titleLabelX = 10000;
-		this.inventoryLabelY = 10000;
 	}
 
 	@Override
@@ -87,6 +93,7 @@ public class WaveformScreen extends AbstractContainerScreen<WaveformMenu>
 		{
 			Probe channel = this.allProbes.get(i);
 
+			this.renderMousePosition(pGuiGraphics, pMouseX, pMouseY, columnNumber);
 			this.renderChannelSignals(pGuiGraphics, channel.loggedSignals, columnNumber);
 			this.renderChannelName(pGuiGraphics, channel.name, columnNumber);
 
@@ -94,10 +101,36 @@ public class WaveformScreen extends AbstractContainerScreen<WaveformMenu>
 		}
 	}
 
-	private void renderChannelSignals(GuiGraphics gui, ArrayList<Boolean> signals, int channelNumber)
+	private void renderMousePosition(GuiGraphics gui, int mouseX, int mouseY, int column)
 	{
-		int baseX = CHANNELS_LEFT + (int)(this.scrollAmount + this.dragAmountX);
-		int baseY = CHANNELS_TOP + CHANNEL_HEIGHT * (channelNumber + 1) - WAVE_BOTTOM_MARGIN;
+		if(mouseX < CHANNELS_LEFT || mouseX > CHANNELS_RIGHT)
+		{
+			return;
+		}
+
+		// draw vertical bar
+		int baseX = mouseX - CHANNELS_LEFT;
+		baseX -= baseX % WIDTH_PER_SIGNAL;
+		baseX += ((int)this.scrollAmountX) % WIDTH_PER_SIGNAL;
+		baseX += CHANNELS_LEFT;
+
+		int baseY = CHANNELS_TOP + CHANNEL_HEIGHT * column;
+
+		gui.blit(WAVEFORM_TEXTURE, baseX, baseY, 245, 0, WIDTH_PER_SIGNAL, CHANNEL_HEIGHT-2);
+
+//		// display redstone tick number
+//		int redstoneTick = mouseX - CHANNELS_LEFT - ((int)this.scrollAmountX);
+//		redstoneTick /= WIDTH_PER_SIGNAL;
+//
+//		String infoToDisplay = String.format("%d", redstoneTick);
+//
+//		gui.drawString(this.font, infoToDisplay, baseX - this.font.width(infoToDisplay)/2, this.topPos - 20, 0xf0f0f0);
+	}
+
+	private void renderChannelSignals(GuiGraphics gui, ArrayList<Boolean> signals, int column)
+	{
+		int baseX = CHANNELS_LEFT + (int)(this.scrollAmountX);
+		int baseY = CHANNELS_TOP + CHANNEL_HEIGHT * (column + 1) - WAVE_BOTTOM_MARGIN;
 
 		for(boolean isPowered : signals)
 		{
@@ -116,19 +149,19 @@ public class WaveformScreen extends AbstractContainerScreen<WaveformMenu>
 
 			if(isPowered)
 			{
-				gui.blit(WAVEFORM_TEXTURE, baseX, baseY -HIGH_WAVE_HEIGHT+ 1, 240, 0, WIDTH_PER_SIGNAL, HIGH_WAVE_HEIGHT);
+				gui.blit(WAVEFORM_TEXTURE, baseX, baseY - HIGH_WAVE_HEIGHT + 1, 240, 0, WIDTH_PER_SIGNAL, HIGH_WAVE_HEIGHT);
 			}
 			else
 			{
-				gui.blit(WAVEFORM_TEXTURE, baseX, baseY -LOW_WAVE_HEIGHT+ 1, 240, 0, WIDTH_PER_SIGNAL, LOW_WAVE_HEIGHT);
+				gui.blit(WAVEFORM_TEXTURE, baseX, baseY - LOW_WAVE_HEIGHT + 1, 240, 0, WIDTH_PER_SIGNAL, LOW_WAVE_HEIGHT);
 			}
 		}
 	}
 
-	private void renderChannelName(GuiGraphics gui, String name, int channelNumber)
+	private void renderChannelName(GuiGraphics gui, String name, int column)
 	{
 		int baseX = CHANNELS_LEFT + 1;
-		int baseY = CHANNELS_TOP + CHANNEL_HEIGHT * channelNumber + 1;
+		int baseY = CHANNELS_TOP + CHANNEL_HEIGHT * column + 1;
 
 		gui.drawString(this.font, name, baseX, baseY, 0xf0f0f0);
 	}
@@ -153,15 +186,18 @@ public class WaveformScreen extends AbstractContainerScreen<WaveformMenu>
 	@Override
 	public boolean mouseScrolled(double pMouseX, double pMouseY, double pDelta)
 	{
-		this.scrollAmount += pDelta * SCROLL_MULTIPLIER;
+		if(!this.allProbes.isEmpty())
+		{
+			this.scrollAmountX += pDelta * SCROLL_MULTIPLIER;
 
-		if(this.scrollAmount > 83)
-		{
-			this.scrollAmount = 83;
-		}
-		else if(this.scrollAmount < 83 - this.allProbes.get(0).loggedSignals.size() * WIDTH_PER_SIGNAL)
-		{
-			this.scrollAmount = 83 - this.allProbes.get(0).loggedSignals.size() * WIDTH_PER_SIGNAL;
+			if(this.scrollAmountX > HOR_SCROLL_LIMIT)
+			{
+				this.scrollAmountX = HOR_SCROLL_LIMIT;
+			}
+			else if(this.scrollAmountX < HOR_SCROLL_LIMIT - this.allProbes.get(0).loggedSignals.size() * WIDTH_PER_SIGNAL)
+			{
+				this.scrollAmountX = HOR_SCROLL_LIMIT - this.allProbes.get(0).loggedSignals.size() * WIDTH_PER_SIGNAL;
+			}
 		}
 
 		return super.mouseScrolled(pMouseX, pMouseY, pDelta);
